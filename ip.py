9f2dbab18fb9af576c90bdae4126d7dce3d9cdeb
Matt Behrens <askedrelic@gmail.com>
2016-02-15 21:26:41 +0100

    fix indents, return 404 when not on root path

--- ip.py
+++ ip.py
@@ -4,16 +4,22 @@ from wsgiref.simple_server import make_server
 
 
 def get_ip(environ, start_response):
-  status = '200 OK'
-  response_headers = [('Content-type', 'text/plain')]
-  start_response(status, response_headers)
-  if 'HTTP_X_FORWARDED_FOR' in environ:
-    return [environ['HTTP_X_FORWARDED_FOR'].split(',')[0]]
-  else:
-    return [environ['REMOTE_ADDR']]
+    response_headers = [('Content-type', 'text/plain')]
+
+    if environ['PATH_INFO'] != '/':
+        status = '404 Not Found'
+        start_response(status, response_headers)
+        return ['ERROR 404']
+    else:
+        status = '200 OK'
+        start_response(status, response_headers)
+        if 'HTTP_X_FORWARDED_FOR' in environ:
+            return [environ['HTTP_X_FORWARDED_FOR'].split(',')[0]]
+        else:
+            return [environ['REMOTE_ADDR']]
 
 
 if __name__ == '__main__':
-  httpd = make_server('', 8000, get_ip)
-  print 'Serving HTTP on port 8000...'
-  httpd.serve_forever()
+    httpd = make_server('', 5000, get_ip)
+    print 'Serving HTTP on port 5000...'
+    httpd.serve_forever()
